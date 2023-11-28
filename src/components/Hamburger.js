b73da50ca321c9b84524d8116e1ef32e646e79e4
BeyondCake <93342461+BeyondCake@users.noreply.github.com>
2023-11-27 22:31:22 -0800

    hamburger fix

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -1,3 +1,4 @@
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import "../styles/styles.scss"
 import { useState, link } from "react";
 
@@ -20,12 +21,45 @@ function Hamburger(){
             <div className="menu-dropdown">
               <ul className="menu-list">
                 {/* <link to="/home"><li>Home</li></link> */}
-                <li>Home</li>
-                <li>About</li>
-                <li>Blinds</li>
-                <li>Screens</li>
-                <li>Curtains</li>
-                <li>Contact</li>
+                <Link to="/home"><li>Home</li></Link>
+                      <Link to="/aboutUs"><li>About</li></Link>
+                      <Link to="/blinds">
+                        <li>Blinds
+                        <ul class="submenu-1">
+                          <li><Link to="/zebraBlinds">Zebra Blinds</Link></li>
+                          <li><Link to="/rollerBlinds">Roller Blinds</Link></li>
+                          <li><Link to="/fauxWoodBlinds">Fauxwood Blinds</Link></li>
+                          <li><Link to="/honeyCombBlinds">Honey Comb Blinds</Link></li>
+                          <li><Link to="/viennaBlinds">Vienna Blinds</Link></li>
+                          <li><Link to="/solarBlinds">Solar Blinds</Link></li>
+                        </ul>
+                        </li>
+                        </Link>
+                       <Link to="">
+                        <li>Screens
+                       <ul class="submenu-1">
+                          <li><a href="">Windows</a>
+                            <ul className='submenu-2'>
+                              <li><a href="">Sliding Window</a></li>
+                              <li><a href="">Handle Push Out window</a></li>
+                              <li><a href="">Lip Bar Screen</a></li>
+                              <li><a href="">Tilt N Window</a></li>
+                            </ul>
+                          </li>
+                          <li><a href="">Doors</a>
+                            <ul className='submenu-2'>
+                              <li><a href="">Patio Slider Screen</a></li>
+                              <li><a href="">Retractable Single Entry Screen</a></li>
+                              <li><a href="">Retractable French Door Screen</a></li>
+                              <li><a href="">Strom Door Screen</a></li>
+                            </ul>
+                          </li>
+                          {/* <li><a href="">Screens 3</a></li>
+                          <li><a href="">Screens 4</a></li> */}
+                        </ul>
+                        </li></Link>
+                       <Link to="/curtains"><li>Curtains</li></Link>             
+                       <Link to="/contact"><li>Contact</li></Link> 
               </ul>
             </div> : "" }
         </div></>
